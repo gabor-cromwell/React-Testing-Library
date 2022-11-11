fd5d4a0d9a4b8f58789b7b88cc978e81d7012d56
Havasi <gabor.havasi@cromwell.co.uk>
2022-11-11 16:17:10 +0000

    Footer test was added

--- net-ninja/src/components/TodoFooter/ToDoFooter.test.js
+++ net-ninja/src/components/TodoFooter/ToDoFooter.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
+import TodoFooter from "./TodoFooter";
 
-test("Check the title exist", async () => {
-  render(<Header title="Header" />);
-  const headerElement = screen.getByText(/header/i);
-  expect(headerElement).toBeInTheDocument();
+const MockToDoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+it("Rendering the correct amount of incompleted tasks", async () => {
+  render(<MockToDoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
 });
