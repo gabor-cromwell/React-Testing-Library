e3dfbd7f716505127dc6a3fc8c4756323820609f
Havasi <gabor.havasi@cromwell.co.uk>
2022-11-14 17:00:03 +0000

    Toto integration test first part

--- net-ninja/src/components/Todo/Todo.test.js
+++ net-ninja/src/components/Todo/Todo.test.js
@@ -9,11 +9,29 @@ const MockTodo = () => {
     </BrowserRouter>
   );
 };
+
+const addToTodoList = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  const buttonElement = screen.getByRole("button", { name: "Add" });
+  tasks.forEach((task) => {
+    fireEvent.change(inputElement, { target: { value: task } });
+    fireEvent.click(buttonElement);
+  });
+};
 describe("Todo test", () => {
-  it("Selecting input field and button?", async () => {
+  it("Checking of task added to the list", async () => {
     render(<MockTodo />);
-    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-    const buttonElement = screen.getByRole("button", { name: "Add" });
-    // expect(inputElement).toBeInTheDocument();
+    addToTodoList(["First", "Second", "Third", "Fourth"]);
+    expect(screen.getAllByTestId("Task container")).toHaveLength(4);
+    // const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    // const buttonElement = screen.getByRole("button", { name: "Add" });
+    // fireEvent.change(inputElement, { target: { value: "First" } });
+    // fireEvent.click(buttonElement);
+    // fireEvent.change(inputElement, { target: { value: "Second" } });
+    // fireEvent.click(buttonElement);
+    //     const divElement_1 = screen.getByText(/first/i);
+    //     const divElement_2 = screen.getByText(/second/i);
+    //     expect(divElement_1).toBeInTheDocument();
+    //     expect(divElement_2).toBeInTheDocument();
   });
 });
